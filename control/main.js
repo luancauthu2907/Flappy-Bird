71a6eeff3c6095a1c98f2ad5114db0857cfd2e03
luancauthu2907 <luancauthu@gmail.com>
2021-03-03 17:31:35 +0700

    update Ngay` 3/3

--- control/main.js
+++ control/main.js
@@ -4,39 +4,30 @@ let canvas =document.getElementById("gameZone");
  let context= canvas.getContext("2d");
 let diem =document.getElementById("score");
 let diem1=0;
+;let dem2=0.5;
+let speed=1; 
 let audio =new Audio();
 audio.src='../nhac/1.mp3'
 const conchim=new Image();
 const ongduoi=new Image();
 const ongtren=new Image();
-const hinhnen =new Image();
+const hinhnen =new Image(); 
 const khoangcachong=300;
 conchim.src="img/bird.png";
 ongduoi.src="img/ongduoi.png";
 ongtren.src="img/ongtren.png";
 hinhnen.src="img/nenchinh.png";
-
-
-
 let bird1 = new bird(canvas.width,canvas.height);
 const run = () => 
-{
-        
+{  
     context.drawImage(hinhnen,0,0);
     context.drawImage(conchim,bird1.x,bird1.y);
-    
-    
     for(let i=0;i<=ongcong.length-1;i++)  
     {
-       
         console.log(ongcong[0].x);
         context.drawImage(ongtren,ongcong[i].x,ongcong[i].y-700);
         context.drawImage(ongduoi,ongcong[i].x,ongcong[i].y+khoangcachong);
-      
-            ongcong[i].x-=1;
-          
-    
-      
+            ongcong[i].x-=speed;
         if(ongcong[i].x == 1260/2) 
         {
             ongcong.push({x:1260, 
@@ -47,8 +38,7 @@ const run = () =>
         {
             ongcong.splice(0,1);
         }
-        console.log(conchim.height);
-         if((bird1.y+conchim.height+1)>=canvas.height||bird1.y-conchim.height-1<=0)     
+         if((bird1.y+conchim.height+1)>=canvas.height||bird1.y-conchim.height<=0)     
          {       
             audio.pause();
              return;        
@@ -57,20 +47,26 @@ const run = () =>
          {
              audio.pause();
               return;  
+         }  
+         if(bird1.x ==ongcong[i].x)  
+         {
+             diem1++;
+             diem.innerText="SCORE : "+diem1;
          }     
-     
+         if(diem1>=5)
+         {
+             dem2=0.6;  
+             speed=1.5;
+
+         }
+         
     } 
     
     requestAnimationFrame(run); 
     for(var j=0 ;j<=2;j++)
     {
-        bird1.y+=0.5;   
+        bird1.y+=dem2;   
     }
-    
-    
-    
-  
-   
     audio.play();       
 }
 document.addEventListener("keydown",function()
@@ -79,7 +75,5 @@ document.addEventListener("keydown",function()
     {
         bird1.y-=20;   
     }
-    
-
 });        
 run(); 
